0075527dc341d9a87e5e6c3872f36ab5a492fd16
Петър Петров <ipetrovbg@gmail.com>
2016-07-19 17:22:43 +0300

    make change

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 require 'pscan.php';
 
 $scan = new pscandir();
-$scan->setConfiqSmall(150);
-
-$scan->scan(__DIR__ . DIRECTORY_SEPARATOR);
+$scan->changeExistingSizeByKey('thumb', array('folder_name'=>'changed-thumb', 'folder_size'=>800));
+//$scan->deleteFolderByName('larg');
+//$scan->addSize(array('folder_name'=>'new-petar', 'folder_size'=>200, 'crop'=>true));
+//$scan->scanAndResize(__DIR__ . DIRECTORY_SEPARATOR);
+echo '<pre>';
+print_r($scan->scan(__DIR__ . DIRECTORY_SEPARATOR));
+echo '</pre>';
--- pscan.php
+++ pscan.php
@@ -5,235 +5,225 @@ require 'vendor/autoload.php';
 // import the Intervention Image Manager Class
 use Intervention\Image\ImageManagerStatic as Image;
 
-class pscandir {
+class pscandir
+{
 
-	private $_confiq_larg 	= 1080;
-	private $_confiq_thumb 	= 500;
-	private $_confiq_small 	= 150;
+    private $_check_file = 'check';
+    private $_folders = [
+        array(
+            'folder_name' => 'thumb',
+            'folder_size' => 600,
+            'crop' => true
+        )
+    ];
 
-	public function setConfiqLarg($value){
-		$this->_confiq_larg = $value;
-	}
+    public function scanAndResize($base = '', &$data = array())
+    {
 
-	public function setConfiqThumb($value){
-		$this->_confiq_thumb = $value;
-	}
+        $array = array_diff(scandir($base), array('.', '..')); # remove ' and .. from the array */
 
-	public function setConfiqSmall($value){
-		$this->_confiq_small = $value;
-	}
+        foreach ($array as $item) {
+            if (is_dir($base . $item)) {
+                $data[$item] = $this->scanAndResize($base . $item . DIRECTORY_SEPARATOR, $data[$item]);
+            } else {
+                $data[] = $base . $item;
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                $type = finfo_file($finfo, $base . $item);
+                finfo_close($finfo);
 
-	public function scan($base='', &$data=array()){
+                if ($type == 'image/jpeg' || $type == 'image/png' || $type == 'image/gif') {
 
-	$array = array_diff(scandir($base), array('.', '..')); # remove ' and .. from the array */
-	$counter = 0;
-	foreach ($array as $item) {
-		if(is_dir($base . $item)){
-			$data[$item] = $this->scan($base . $item. DIRECTORY_SEPARATOR, $data[$item]);
-		}else{
-			$data[] = $base . $item;
-			$finfo = finfo_open(FILEINFO_MIME_TYPE);
-			$type = finfo_file($finfo, $base . $item);
-			finfo_close($finfo);
+                    foreach ($this->_folders as $folder) {
+                        if (!is_dir($base . DIRECTORY_SEPARATOR . $folder['folder_name'])) {
+                            if (!file_exists($base . DIRECTORY_SEPARATOR . $this->_check_file . '.txt')) {
+                                if (!mkdir($base . DIRECTORY_SEPARATOR . $folder['folder_name'], 0777, true)) {
+                                    die('Failed to create folders...');
+                                } else {
+                                    print "creating  \" " . $folder['folder_name'] . " \" folder \n";
+                                    flush();
 
-			if($type == 'image/jpeg' || $type == 'image/png' || $type == 'image/gif'){
-				if(!is_dir($base . DIRECTORY_SEPARATOR . 'larg')){
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if (!mkdir($base . DIRECTORY_SEPARATOR . 'larg', 0777, true)) {
-							    die('Failed to create folders...');
-						}else{
-							print "create directory \"larg\" \n";
-    						flush();
-							$content = "";
-							$fp = fopen( $base . DIRECTORY_SEPARATOR . 'larg' . DIRECTORY_SEPARATOR . "check.txt","wb");
-							fwrite($fp,$content);
-							fclose($fp);
+                                    $content = "";
+                                    $fp = fopen($base . DIRECTORY_SEPARATOR . $folder['folder_name'] . DIRECTORY_SEPARATOR . $this->_check_file . '.txt', "wb");
+                                    fwrite($fp, $content);
+                                    fclose($fp);
 
-							print "create file \"check.txt\" \n";
-    						flush();
-						}
-					}
-					
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if(!file_exists($base . DIRECTORY_SEPARATOR . 'larg' . DIRECTORY_SEPARATOR . $item)){
-							copy($base . $item, $base . DIRECTORY_SEPARATOR . 'larg' . DIRECTORY_SEPARATOR . $item);
-							print "copy file \" $base . $item  into larg folder \" \n ";
-    						flush();
-							$image = Image::make($base . DIRECTORY_SEPARATOR . 'larg' . DIRECTORY_SEPARATOR . $item);
-							$image->resize(null, $this->_confiq_larg, function ($constraint) {
-							    $constraint->aspectRatio();
-							    $constraint->upsize();
-							})->save();
-						}
-					}
+                                    print "creating  \" check.txt \" into " . $folder['folder_name'] . " folder \n ";
+                                    flush();
+                                }
+                            }
 
-				}else{
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if(!file_exists($base . DIRECTORY_SEPARATOR . 'larg' . DIRECTORY_SEPARATOR . $item)){
-							copy($base . $item, $base . DIRECTORY_SEPARATOR . 'larg' . DIRECTORY_SEPARATOR . $item);
+                            if (!file_exists($base . DIRECTORY_SEPARATOR . $this->_check_file . '.txt')) {
+                                if (!file_exists($base . DIRECTORY_SEPARATOR . $folder['folder_name'] . DIRECTORY_SEPARATOR . $item)) {
+                                    copy($base . $item, $base . DIRECTORY_SEPARATOR . $folder['folder_name'] . DIRECTORY_SEPARATOR . $item);
 
-							print "copy file \" $base . $item  into larg folder \" \n";
-    						flush();
+                                    print "copy file \" $base . $item  into " . $folder['folder_name'] . " folder \" \n";
+                                    flush();
 
-							$image = Image::make($base . DIRECTORY_SEPARATOR . 'larg' . DIRECTORY_SEPARATOR . $item);
-							$image->resize(null, $this->_confiq_larg, function ($constraint) {
-							    $constraint->aspectRatio();
-							    $constraint->upsize();
-							})->save();
-						}
-					}
-				}
+                                    $image = Image::make($base . DIRECTORY_SEPARATOR . $folder['folder_name'] . DIRECTORY_SEPARATOR . $item);
+                                    if (array_key_exists('crop', $folder) && $image->width() > $folder['folder_size']) {
 
-				if(!is_dir($base . DIRECTORY_SEPARATOR . 'thumb')){
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if (!mkdir($base . DIRECTORY_SEPARATOR . 'thumb', 0777, true)) {
-							    die('Failed to create folders...');
-						}else{
-							print "creating  \" thumb \" folder \n";
-    						flush();
+                                        $width = $image->width();
+                                        $height = $image->height();
 
-							$content = "";
-							$fp = fopen( $base . DIRECTORY_SEPARATOR . 'thumb' . DIRECTORY_SEPARATOR . "check.txt","wb");
-							fwrite($fp,$content);
-							fclose($fp);
+                                        if ($width > $height) {
+                                            $newImage = $image->resize(null, $folder['folder_size'], function ($constraint) {
+                                                $constraint->aspectRatio();
+                                                $constraint->upsize();
+                                            })->save();
 
-							print "creating  \" check.txt \" into thumb folder \n ";
-    						flush();
-						}
-					}
-					
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if(!file_exists($base . DIRECTORY_SEPARATOR . 'thumb' . DIRECTORY_SEPARATOR . $item)){
-							copy($base . $item, $base . DIRECTORY_SEPARATOR . 'thumb' . DIRECTORY_SEPARATOR . $item);
+                                            $newWidth = $newImage->width();
+                                            $newImage->crop($folder['folder_size'], $folder['folder_size'], round(($newWidth - $folder['folder_size']) / 2), 0)->save();
 
-							print "copy file \" $base . $item  into thumb folder \" \n";
-    						flush();
+                                        } else {
+                                            $newImage = $image->resize($folder['folder_size'], null, function ($constraint) {
+                                                $constraint->aspectRatio();
+                                                $constraint->upsize();
+                                            })->save();
 
-							$image = Image::make($base . DIRECTORY_SEPARATOR . 'thumb' . DIRECTORY_SEPARATOR . $item);
-							$image->resize(null, $this->_confiq_thumb, function ($constraint) {
-							    $constraint->aspectRatio();
-							    $constraint->upsize();
-							})->save();
-							print "resize image \" $base . $item   \" into thumb folder \n";
-    						flush();
-						}
-					}
+                                            $newHeight = $newImage->height();
+                                            $newImage->crop($folder['folder_size'], $folder['folder_size'], 0, round(($newHeight - $folder['folder_size']) / 2))->save();
+                                        }
 
-				}else{
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if(!file_exists($base . DIRECTORY_SEPARATOR . 'thumb' . DIRECTORY_SEPARATOR . $item)){
-							copy($base . $item, $base . DIRECTORY_SEPARATOR . 'thumb' . DIRECTORY_SEPARATOR . $item);
+                                    } else {
+                                        $image->resize(null, $folder['folder_size'], function ($constraint) {
+                                            $constraint->aspectRatio();
+                                            $constraint->upsize();
+                                        })->save();
+                                    }
+                                    print "resize image \" $base . $item   \" into " . $folder['folder_name'] . " folder \n";
+                                    flush();
+                                }
+                            }
 
-							print "copy file \" $base . $item  into thumb folder \" \n";
-    						flush();
+                        } else {
+                            if (!file_exists($base . DIRECTORY_SEPARATOR . $this->_check_file . '.txt')) {
+                                if (!file_exists($base . DIRECTORY_SEPARATOR . $folder['folder_name'] . DIRECTORY_SEPARATOR . $item)) {
+                                    copy($base . $item, $base . DIRECTORY_SEPARATOR . $folder['folder_name'] . DIRECTORY_SEPARATOR . $item);
 
-							$image = Image::make($base . DIRECTORY_SEPARATOR . 'thumb' . DIRECTORY_SEPARATOR . $item);
-							$image->resize(null, $this->_confiq_thumb, function ($constraint) {
-							    $constraint->aspectRatio();
-							    $constraint->upsize();
-							})->save();
-							print "resize image \" $base . $item \" into thumb folder \n";
-    						flush();
-						}
-					}
-				}
+                                    print "copy file \" $base . $item  into " . $folder['folder_name'] . " folder \" \n";
+                                    flush();
+
+                                    $image = Image::make($base . DIRECTORY_SEPARATOR . $folder['folder_name'] . DIRECTORY_SEPARATOR . $item);
+
+                                    if (array_key_exists('crop', $folder) && $image->width() > $folder['folder_size']) {
+
+                                        $width = $image->width();
+                                        $height = $image->height();
+
+                                        if ($width > $height) {
+                                            $newImage = $image->resize(null, $folder['folder_size'], function ($constraint) {
+                                                $constraint->aspectRatio();
+                                                $constraint->upsize();
+                                            })->save();
+
+                                            $newWidth = $newImage->width();
+                                            $newImage->crop($folder['folder_size'], $folder['folder_size'], round(($newWidth - $folder['folder_size']) / 2), 0)->save();
+
+                                        } else {
+                                            $newImage = $image->resize($folder['folder_size'], null, function ($constraint) {
+                                                $constraint->aspectRatio();
+                                                $constraint->upsize();
+                                            })->save();
+
+                                            $newHeight = $newImage->height();
+                                            $newImage->crop($folder['folder_size'], $folder['folder_size'], 0, round(($newHeight - $folder['folder_size']) / 2))->save();
+                                        }
+
+                                    } else {
+                                        $image->resize(null, $folder['folder_size'], function ($constraint) {
+                                            $constraint->aspectRatio();
+                                            $constraint->upsize();
+                                        })->save();
+                                    }
 
 
+                                    print "resize image \" $base . $item \" into " . $folder['folder_name'] . " folder \n";
+                                    flush();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 
-				if(!is_dir($base . DIRECTORY_SEPARATOR . 'small')){
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if (!mkdir($base . DIRECTORY_SEPARATOR . 'small', 0777, true)) {
-							    die('Failed to create folders...');
-						}else{
+        }
+        return $data; // return the $data array
 
-							print "create directory \" small \" \n";
-    						flush();
+    }
 
-							$content = "";
-							$fp = fopen( $base . DIRECTORY_SEPARATOR . 'small' . DIRECTORY_SEPARATOR . "check.txt","wb");
-							fwrite($fp,$content);
-							fclose($fp);
+    public function scan($base = '', &$data = array())
+    {
+        $array = array_diff(scandir($base), array('.', '..')); # remove ' and .. from the array */
 
-							print "create \" check.txt \" file \n";
-    						flush();
-						}
-					}
-					
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if(!file_exists($base . DIRECTORY_SEPARATOR . 'small' . DIRECTORY_SEPARATOR . $item)){
-							copy($base . $item, $base . DIRECTORY_SEPARATOR . 'small' . DIRECTORY_SEPARATOR . $item);
+        foreach ($array as $item) {
+            if (is_dir($base . $item)) {
+                $data[$item] = $this->scan($base . $item . DIRECTORY_SEPARATOR, $data[$item]);
+            } else {
+                $data[] = $base . $item;
+            }
 
-							print "copying \" $base . $item \" image \n";
-    						flush();
+        }
 
-							$image = Image::make($base . DIRECTORY_SEPARATOR . 'small' . DIRECTORY_SEPARATOR . $item);
+        return $data; // return the $data array
+    }
 
-							$width = $image->width();
-							$height = $image->height();
+    public function getConfiqCheckFile()
+    {
+        return $this->_check_file;
+    }
 
-							if($width > $height){
-								$newImage = $image->resize(null, $this->_confiq_small, function ($constraint) {
-								    $constraint->aspectRatio();
-								    $constraint->upsize();
-								})->save();
 
-								$newWidth = $newImage->width();
-								$newImage->crop($this->_confiq_small, $this->_confiq_small, round(($newWidth - $this->_confiq_small)/2), 0)->save();
+    public function getFolders()
+    {
+        return $this->_folders;
+    }
 
-							}else{
-								$newImage = $image->resize($this->_confiq_small, null, function ($constraint) {
-								    $constraint->aspectRatio();
-								    $constraint->upsize();
-								})->save();
+    public function deleteFolderByKey($key)
+    {
+        if ($key) {
+            unset($this->_folders[$key]);
+        }
+    }
 
-								$newHeight = $newImage->height();
-								$newImage->crop($this->_confiq_small, $this->_confiq_small, 0, round(($newHeight - $this->_confiq_small)/2) )->save();
-							}
-						}
-					}
+    public function deleteFolderByName($folder_name)
+    {
+        foreach ($this->_folders as $key => $folder) {
+            if ($folder['folder_name'] == $folder_name) {
+                unset($this->_folders[$key]);
+            }
+        }
+    }
 
-				}else{
-					if(!file_exists($base . DIRECTORY_SEPARATOR . 'check.txt')){
-						if(!file_exists($base . DIRECTORY_SEPARATOR . 'small' . DIRECTORY_SEPARATOR . $item)){
-							copy($base . $item, $base . DIRECTORY_SEPARATOR . 'small' . DIRECTORY_SEPARATOR . $item);
-							$image = Image::make($base . DIRECTORY_SEPARATOR . 'small' . DIRECTORY_SEPARATOR . $item);
+    public function addSize($array)
+    {
+        if (is_array($array)) {
+            if (array_key_exists('folder_name', $array) && array_key_exists('folder_size', $array)) {
+                array_push($this->_folders, $array);
+            }
+        } else {
+            echo "Incorect format for addSize method. Must be array('folder_name'=>'some-name', 'folder_size'=>500);";
+        }
+    }
 
-							$width = $image->width();
-							$height = $image->height();
-
-							if($width >= $height){
-								$newImage = $image->resize(null, $this->_confiq_small, function ($constraint) {
-								    $constraint->aspectRatio();
-								    $constraint->upsize();
-								})->save();
-
-								$newWidth = $newImage->width();
-								$newImage->crop($this->_confiq_small, $this->_confiq_small,  round(($newWidth - $this->_confiq_small)/2), 0)->save();
-
-							}else{
-								$newImage = $image->resize($this->_confiq_small, null, function ($constraint) {
-								    $constraint->aspectRatio();
-								    $constraint->upsize();
-								})->save();
-
-								$newHeight = $newImage->height();
-								$newImage->crop($this->_confiq_small, $this->_confiq_small, 0, round(($newHeight - $this->_confiq_small)/2) )->save();
-							}
-
-							
-						}
-					}
-				}
-								
-			}
-		}
-
-		$counter++;
-	}
-	return $data; // return the $data array
-
-	}
-
+    public function changeExistingSizeByKey($key, $new_size)
+    {
+        if ($key && $new_size) {
+            if (is_array($new_size)) {
+                foreach ($this->_folders as $k => $folder) {
+                    if ($folder['folder_name'] == $key) {
+                        if (array_key_exists('folder_name', $new_size) && array_key_exists('folder_size', $new_size)) {
+                            $this->_folders[$k]['folder_name'] = $new_size['folder_name'];
+                            $this->_folders[$k]['folder_size'] = $new_size['folder_size'];
+                        }
+                    }
+                }
+            } else {
+                echo "In changeExistingSizeByKey() second parameter must be array";
+            }
+        } else {
+            echo "changeExistingSizeByKey() require 2 parameters. Key as string and New size as array";
+        }
+    }
 }
 
 
